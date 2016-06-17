f93cfbf9baca398ca908a6424b28bd9b26e789a0
Philippe Luchansky <philuchansky@gmail.com>
2016-06-17 10:45:26 -0700

    add express and logger with listener to server.js

--- server.js
+++ server.js
@@ -1,2 +1,10 @@
 // Initial commit
 // here's where code goes
+var express = require('express')
+var logger  = require('morgan')
+var app = express()
+
+app.listen(3000, function(err){
+  if(err) return console.log(err)
+  console.log("Server running on 3000. Huzahhh. lolz.")
+})
