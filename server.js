9db9cabb8d73b48c88c814d0b25c8b442869c517
Michael Ray <raymichael23@gmail.com>
2016-06-17 10:55:00 -0700

    added packages for body-parser and mongoose

--- server.js
+++ server.js
@@ -3,6 +3,8 @@
 var express = require('express')
 var logger  = require('morgan')
 var app = express()
+var bodyParser = require('body-parser')
+var mongoose = require('mongoose')
 
 app.listen(3000, function(err){
   if(err) return console.log(err)
